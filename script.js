8cff20d36f426884d0a7d505456f035bad1c8f61
enstrONGO <75175712+enstrong@users.noreply.github.com>
2021-04-24 21:58:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-location.href = 'https://www.google.com/search?q=ok+boomer&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi2p7iqnpfwAhWts4sKHcMCB74Q_AUoAXoECAEQAw'
+location.href = 'https://www.google.com/search?q=gay+porn&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi2p7iqnpfwAhWts4sKHcMCB74Q_AUoAXoECAEQAw'
 
