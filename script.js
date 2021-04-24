be5cace1dab338642beda3a4393e43f1bcd8ab9a
enstrONGO <75175712+enstrong@users.noreply.github.com>
2021-04-24 21:47:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-location.href = "https://www.google.com/search?q=ok+boomer&sxsrf=ALeKk01z5KtAsScE02lSi9iLDOstTxUa5Q:1619278937439&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiIsO6OnJfwAhXjxIsKHT08D8AQ_AUoAXoECAEQAw&
+location.href = "https://www.google.com/search?q=ok+boomer&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjD6ZaDnZfwAhVKiYsKHa6lCl8Q_AUoAXoECAEQAw"
